330e3c0d774a04876bfa01760fc60a014cc29add
tharsila <tharsilapaula@gmail.com>
2022-04-26 23:31:33 -0300

    add conteudo do game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,31 @@ import './Game.scss';
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick = {verifyLetter}>Finalizar o jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">Dica sobre a palavra: <span>Dica...</span></h3>
+
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button onClick = {verifyLetter}>Jogar</button>
+        </form>
+      </div>
+
+      <div className="wrongLettersContainer">
+        <p>Letras já utlizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
