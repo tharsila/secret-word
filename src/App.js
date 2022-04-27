517f07de9d072ca68e60ca62646edbed181ef42f
tharsila <tharsilapaula@gmail.com>
2022-04-26 22:27:20 -0300

    add função  escolher palavra e categoria aleatoria

--- src/App.js
+++ src/App.js
@@ -22,13 +22,53 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words)
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    /* pick a random category */
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    
+    /* pick a random word */
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return {word, category}
+  }
+
+  /* stars the game */
+  const startGame = () => {
+    /* pick word and pick category*/
+    const {word, category} = pickWordAndCategory();
+
+    /* create an array of letters*/
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((letter) => (letter.toLowerCase()));
+    
+    /* fill states */
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  }
+
+  /* process the letter input */
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  /* restart the game */
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className="App">
-     {gameStage === "start" &&  <StartScreen />}
-     {gameStage === "game" && <Game />}
-     {gameStage === "end" && <GameOver />}
+     {gameStage === "start" &&  <StartScreen startGame = {startGame}/>}
+     {gameStage === "game" && <Game verifyLetter = {verifyLetter}/>}
+     {gameStage === "end" && <GameOver retry = {retry}/>}
     </div>
   );
 }
