1e144cbff36144deb4189ae73f591102a514ca87
tharsila <tharsilapaula@gmail.com>
2022-04-27 21:33:06 -0300

    add novos states e props

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses,setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   const pickWordAndCategory = () => {
     /* pick a random category */
     const categories = Object.keys(words);
@@ -49,7 +54,7 @@ function App() {
     /* fill states */
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   }
@@ -67,7 +72,17 @@ function App() {
   return (
     <div className="App">
      {gameStage === "start" &&  <StartScreen startGame = {startGame}/>}
-     {gameStage === "game" && <Game verifyLetter = {verifyLetter}/>}
+     {gameStage === "game" && 
+     <Game
+      verifyLetter = {verifyLetter}
+      pickedWord = {pickedWord}
+      pickedCategory = {pickedCategory}
+      letters = {letters}
+      guessedLetters = {guessedLetters}
+      wrongLetters = {wrongLetters}
+      guesses = {guesses}
+      score = {score}
+     />}  
      {gameStage === "end" && <GameOver retry = {retry}/>}
     </div>
   );
