c20c9b259febc4a905f1d40e51a2749d30b23d01
tharsila <tharsilapaula@gmail.com>
2022-05-02 22:51:42 -0300

    add prop score

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,9 +1,10 @@
 import './GameOver.scss'
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
     <div>
-      <h1>GameOver</h1>
+      <h1>Fim do jogo!</h1>
+      <h2>Sua pontuação foi: <span>{score}</span></h2>
       <button onClick = {retry}>Resetar jogo</button>
     </div>
   )
