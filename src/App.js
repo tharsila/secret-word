59eeb9a46977ea2c83ba2af250cc2d52595a0cab
tharsila <tharsilapaula@gmail.com>
2022-05-02 22:00:26 -0300

    add logica para fim de jogo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
   {id: 3, name: "end"}
-]
+];
+
+const guessesQuantity = 3;
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
@@ -28,7 +30,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses,setGuesses] = useState(3);
+  const [guesses,setGuesses] = useState(guessesQuantity);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -77,12 +79,30 @@ function App() {
       setWrongLetters((actualWrongLetters) => [
         ...actualWrongLetters, normalizedLetter,
       ])
-    }
 
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
   })
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  useEffect(() => {
+   if(guesses <= 0) {
+    /* reset all states */
+    clearLetterStates()
+
+    setGameStage(stages[2].name)
+   }
+
+  },[guesses]);
 
   /* restart the game */
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQuantity);
     setGameStage(stages[0].name);
   }
 
